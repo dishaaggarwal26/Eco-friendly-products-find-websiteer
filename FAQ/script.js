8de0c2504544e9a18b4389069e7a9cee1ece8acc
PRANITA GUPTA <pranita2413@gmail.com>
2024-08-03 21:09:47 +0530

    Delete FAQ/script.js

--- FAQ/script.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(element) {
-    const answer = element.nextElementSibling;
-    if (answer.style.display === "block") {
-        answer.style.display = "none";
-    } else {
-        answer.style.display = "block";
-    }
-}
